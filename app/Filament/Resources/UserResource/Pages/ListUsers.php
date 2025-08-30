df13374b2484dc8d53ac94fe3ca38de1619b29c9
Darnes1958 <abdel_ati_m@yahoo.com>
2025-08-30 20:42:02 +0200

    any

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -2,11 +2,14 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Models\User;
 use Filament\Actions\CreateAction;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
 
@@ -20,6 +23,50 @@ protected ?string $heading=' ';
             CreateAction::make()
 
                 ->label('اضافة حالة'),
+        ];
+    }
+    public function getTabs(): array
+    {
+        return [
+          'الكل'=>Tab::make()->badge(fn()=>User::query()->count()),
+            'كاملة'=>Tab::make()->badge(fn()=>User::query()->whereHas('Autistic')->whereHas('Family')
+                ->whereHas('Boy')->whereHas('Growth')
+                ->whereHas('Medicine')->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereHas('Autistic')->whereHas('Family')
+                    ->whereHas('Boy')->whereHas('Growth')
+                    ->whereHas('Medicine')),
+            'جزئية'=>Tab::make()->badge(fn()=>User::query()
+                ->where(function ($q) {
+                    return $q->whereHas('Autistic')->orwhereHas('Family')
+                        ->orwhereHas('Boy')->orwhereHas('Growth')
+                        ->orwhereHas('Medicine');
+                })
+                ->where(function ($q){
+                    return $q->whereDoesntHave('Family')
+                        ->orwhereDoesntHave('Boy')->orwhereDoesntHave('Growth')
+                        ->orwhereDoesntHave('Medicine');
+                })
+                ->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query
+                    ->where(function ($q) {
+                        return $q->whereHas('Autistic')->orwhereHas('Family')
+                            ->orwhereHas('Boy')->orwhereHas('Growth')
+                            ->orwhereHas('Medicine');
+                    })
+                    ->where(function ($q){
+                        return $q->whereDoesntHave('Family')
+                            ->orwhereDoesntHave('Boy')->orwhereDoesntHave('Growth')
+                            ->orwhereDoesntHave('Medicine');
+                    })
+                ),
+            'لم يبدأ'=>Tab::make()->badge(fn()=>User::query()->whereDoesntHave('Autistic')
+                ->whereDoesntHave('Family')
+                ->whereDoesntHave('Boy')->whereDoesntHave('Growth')
+                ->whereDoesntHave('Medicine')->count())
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereDoesntHave('Autistic')
+                        ->whereDoesntHave('Family')
+                        ->whereDoesntHave('Boy')->whereDoesntHave('Growth')
+                        ->whereDoesntHave('Medicine')),
         ];
     }
 }
